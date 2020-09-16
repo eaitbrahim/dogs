178e349dd078fcc6d33a12dbf57defd3058d7597
Elmehdi <eaitbrahim@gmail.com>
2020-09-17 00:34:15 +0100

    Run slideshow every 3 seconds & handle edge cases of images

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let timer;
+let deleteFirstPhotoDelay;
+
 async function start(){
  const response = await fetch('https://dog.ceo/api/breeds/list/all');
  const data = await response.json();
@@ -24,9 +27,41 @@ async function loadByBread(breed){
 }
 
 function createSlideshow(images){
-    document.getElementById('slideshow').innerHTML = `
-    <div class="slide" style="background-image: url('${images[0]}')"></div>
-    `
+    let currentPosition = 0;
+    clearInterval(timer);
+    clearTimeout(deleteFirstPhotoDelay);
+
+    if (images.length > 1){
+        document.getElementById('slideshow').innerHTML = `
+        <div class="slide" style="background-image: url('${images[0]}')"></div>
+        <div class="slide" style="background-image: url('${images[1]}')"></div>
+        `
+
+        currentPosition += 2;
+        if(images.length == 2) {
+            currentPosition = 0;
+        }
+
+        timer = setInterval(nextSlide, 3000);
+    } else {
+        document.getElementById('slideshow').innerHTML = `
+        <div class="slide" style="background-image: url('${images[0]}')"></div>
+        <div class="slide"></div>
+        `
+    }
+
+    function nextSlide(){
+        document.getElementById('slideshow').insertAdjacentHTML('beforeend', `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`);
+        deleteFirstPhotoDelay = setTimeout(function(){
+            document.querySelector('.slide').remove();
+        }, 1000);
+
+        if (currentPosition + 1 >= images.length){
+            currentPosition = 0;
+        } else {
+            currentPosition++;
+        }
+    }
 }
 
 start();
