69892e9cce8cfe0c705e9d209a7d593547372e09
Elmehdi <eaitbrahim@gmail.com>
2020-09-17 00:00:21 +0100

    Fetch images by breed

--- main.js
+++ main.js
@@ -6,13 +6,21 @@ async function start(){
 
 function createBreedList(breedList){
     document.getElementById('breed').innerHTML = `
-    <select>
+    <select onchange="loadByBread(this.value)">
         <option>Choose a dog breed</option>
         ${Object.keys(breedList).map(function(breed){
             return `<option>${breed}</option>`
         }).join('')}
     </select>
     `
+}
+
+async function loadByBread(breed){
+    if(breed != 'Choose a dog breed'){
+        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+        const data = await response.json();
+        console.log(data.message);
+    }
 }
 
 start();
